817f2e21a07bc18825bb5304d32b67f50399b2ac
mecarm <melecarmine99@gmail.com>
2023-01-17 17:39:52 +0100

    Creata tabella con migrate

---
+++ app/Models/TrainModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TrainModel extends Model
+{
+    //
+}
---
+++ database/migrations/2023_01_17_144113_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('color', 100);
+            $table->string('date', 20);
+            $table->string('wagons', 10);
+            $table->smallInteger('racing')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
